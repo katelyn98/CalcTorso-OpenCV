acd19c296ee53720a07c7337925a8ae2198f1e14
Katelyn Morrison <codelikeagirl98@gmail.com>
2019-06-02 18:42:36 -0400

    Update

--- PythonML/ComputerVisionML.py
+++ PythonML/ComputerVisionML.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 print(tf.__version__)
 
 #Loading in the Fashion MNIST data
 mnist = tf.keras.datasets.fashion_mnist
 
-(training_images, training_labels), (test_images, test_labels) = mnist.load_data
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
 #printing a training image and training label to see what the values look like
-import matplotlib.pyplot as plt
 plt.imshow(training_images[0])
 print(training_labels[0])
 print(training_images[0])
